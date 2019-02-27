30135c9d3436deb849d479850af3a9c4af0a7216
ouali <ouali.aghiles@gmail.com>
2019-02-27 04:29:35 +0100

    try gulp

--- source/assets/scripts/app.js
+++ source/assets/scripts/app.js
@@ -1,1 +1,22 @@
-console.log('Hello ');
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'http://localhost:3000/Panels', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400) {
+    data.forEach(todo => {
+        console.log(todo.list);
+    });
+    } else {
+    console.log('error');
+    }
+  }
+
+
+// Send request
+request.send();
